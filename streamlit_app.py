ba788a452a407a4936a8ae591cdc76d19dc65e3a
TARUN BORDOLOI <118963122+TARUNSFLX@users.noreply.github.com>
2022-11-24 13:58:52 +0530

    Update streamlit_app.py
    Added a sub-header line specifying the menu
    Added 3 lines of items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+omega 3 and Blueberry Oatmeal
+Kale, Spinach and Rocket Smoothie
+Hard-Boiled Free-Range Egg
