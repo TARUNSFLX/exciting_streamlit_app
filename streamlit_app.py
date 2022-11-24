548a30aa1226134f2bb65388a6f96ecf1a6016f4
TARUN BORDOLOI <118963122+TARUNSFLX@users.noreply.github.com>
2022-11-24 14:55:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
-omega 3 and Blueberry Oatmeal
+Omega 3 and Blueberry Oatmeal
 Kale, Spinach and Rocket Smoothie
 Hard-Boiled Free-Range Egg
 
