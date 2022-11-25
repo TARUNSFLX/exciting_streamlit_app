7518c63922abfa88fc4aaf6ae3990c46077d54ae
TARUN BORDOLOI <118963122+TARUNSFLX@users.noreply.github.com>
2022-11-25 13:45:26 +0530

    Update streamlit_app.py
    Importing pandas
    Named a dataframe and pulling the data into the dataframe
    streamlit library to display the data

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥚''Hard-Boiled Free-Range Egg')
 streamlit.text('🥑''Avocado Toast')
 
 streamlit.header('🍌''🥭''Build Your Own Fruit Smoothie''🥑''🫐')
+import pandas
+my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
