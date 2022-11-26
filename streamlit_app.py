918d31c39530c0ce9a709b100b08837721004229
TARUN BORDOLOI <118963122+TARUNSFLX@users.noreply.github.com>
2022-11-26 12:26:55 +0530

    Update streamlit_app.py
    Added a statement to pre - populate the list to be printed

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let us put a pick list so that the customer can select the fruit they want
-streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 # Display the table on the page
 streamlit.dataframe(my_fruit_list)
 
