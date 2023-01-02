58557484e66e359cb5b540f3687b89267eff5c1a
TARUN BORDOLOI <118963122+TARUNSFLX@users.noreply.github.com>
2023-01-02 20:24:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,7 @@ streamlit.write('The user entered ', fruit_choice)
 # Don't run anything past here while we troubleshoot
 streamlit.stop()
 
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select*from fruit_load_list")
